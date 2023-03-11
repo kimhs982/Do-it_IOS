c0eb5793dbde91ff875d0f0466eb4304f4fee305
HyungSuk Kim <kimhyungsuk982@gmail.com>
2023-03-11 21:24:58 +0900

    3월 11일
    02장
    Hello World

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 김형석 on 2023/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+}
+
