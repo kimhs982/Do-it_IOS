91449215f2505c8d320ae9a1ae35a2ab848d1bb0
HyungSuk Kim <kimhyungsuk982@gmail.com>
2023-04-05 17:39:00 +0900

    4월 5일
    06장
    Alert
    
    TestApp(연습 프로젝트)

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by 김형석 on 2023/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
