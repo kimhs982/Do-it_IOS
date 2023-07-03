f3303048f060720b6ceff6b85bc15f7f2fbb5920
HyungSuk Kim <kimhyungsuk982@gmail.com>
2023-07-04 00:45:41 +0900

    7월 3일(~ 11-5)
    11장
    Navigation

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by 김형석 on 2023/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            // 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            // 바 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+    }
+
+}
+
