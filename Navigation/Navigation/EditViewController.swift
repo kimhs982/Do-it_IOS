bd0b0284e1bd732b92ce1617381825571932f6e3
HyungSuk Kim <kimhyungsuk982@gmail.com>
2023-07-04 14:49:14 +0900

    7월 4일
    11장
    Navigation

--- Navigation/Navigation/EditViewController.swift
+++ Navigation/Navigation/EditViewController.swift
@@ -7,19 +7,48 @@
 
 import UIKit
 
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+}
+
 class EditViewController: UIViewController {
+    
     var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate: EditDelegate?
+    var isOn = false
+    
     @IBOutlet var lblWay: UILabel!
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swIsOn: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         lblWay.text = textWayValue
+        txMessage.text = textMessage
+        swIsOn.isOn = isOn
     }
     
     @IBAction func btnDone(_ sender: UIButton) {
+        // 메인화면으로 텍스트와 전구 이미지 상태 보내기
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+        }
+        // 메인화면으로 이동하기
         _ = navigationController?.popViewController(animated: true)
+    }
+    
+    // 전구 켜고 끄기
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
     }
     
     /*
